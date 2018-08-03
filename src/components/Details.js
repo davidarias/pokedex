2b99021fabae890fbd684e95394024cc740aabe7
David Arias <arias1011@gmail.com>
2018-08-04 00:35:17 +0200

    added description to details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -13,65 +13,85 @@ function makeEvolutionList(chain, list = []){
         let line = (<li key={chain.species.name}>{chain.species.name} evolves into {chain.evolves_to[0].species.name}</li>);
         list.push(line);
         return makeEvolutionList(chain.evolves_to[0], list);
-    }else{
-        return list;
     }
+
+    return list;
 }
 
 function Evolutions({evolutionChain}){
     if ( ! evolutionChain ){
         return (<img className="evolutions-loading" src={loading} alt="loading"/>);
-
-    }else{
-        let evolutionsList = makeEvolutionList( evolutionChain.chain );
-
-        return (
-            <ul className="evolutions">{evolutionsList}</ul>
-        );
     }
+
+    let evolutionsList = makeEvolutionList( evolutionChain.chain );
+
+    return (
+        <ul>{evolutionsList}</ul>
+    );
+
 }
 
-function Info({pokemon, evolutionChain}){
+function FlavoredText({species}){
+    if (species){
+        let flavoredText = species.flavor_text_entries.find( textEntry => {
+            return textEntry.language.name === 'en';
+        });
+        return <p>{flavoredText.flavor_text}</p>;
+    }
+    return (<img src={loading} alt="loading"/>);
+}
+
+
+function DetailItem({name, value}){
+    return (
+        <div className="row">
+          <div className="col-3">
+            <strong>
+              {name}
+            </strong>
+          </div>
+          <div className="col-9">
+            {value}
+          </div>
+        </div>
+    );
+}
+
+function Info({pokemon, species, evolutionChain}){
     if ( pokemon.id === null ){
         return (<img src={loading} alt="loading"/>);
-    }else{
-        let types = pokemon.types.map( type => {
-            return (
-                <li key={type.type.name}>{type.type.name}</li>
-            );
-        });
-
-        return (
-            <div className="row">
-              <div className="col-3">
-                <Image name={pokemon.name} />
-              </div>
-              <div className="col-9">
-                <ul>
-                  <li><strong>ID:</strong> {pokemon.id}</li>
-                  <li><strong>Types:</strong>
-                    <ul className="types">{types}</ul>
-                  </li>
-                  <li><strong>Evolutions:</strong>
-                    <Evolutions evolutionChain={evolutionChain} />
-                  </li>
-                </ul>
-              </div>
-            </div>
-        );
     }
-}
 
-function getEvolutionChain(pokemon, callback){
-    pokeapi.getPokemonSpeciesByName(pokemon.name).then((response) => {
-        pokeapi.resource(response.evolution_chain.url).then((response) => {
-            callback(response);
-        }).catch((err) => {
-            console.error("Couldn't load pokemon evolution chain :(");
-        });;
-    }).catch((err) => {
-        console.error("Couldn't load pokemon species :(");
-    });;
+    let types =(
+        <ul>
+          {pokemon.types.map( type => {
+              return (
+                  <li key={type.type.name}>{type.type.name}</li>
+              );
+          })}
+        </ul>
+
+    );
+
+    let evolutions = <Evolutions evolutionChain={evolutionChain} />;
+
+    return (
+        <div className="row">
+          <div className="col-3">
+            <Image name={pokemon.name} />
+          </div>
+          <div className="col-9">
+
+            <ul>
+              <li><DetailItem name={"ID:"} value={pokemon.id} /></li>
+              <li><DetailItem name={"Description:"} value={FlavoredText({species})} /></li>
+              <li><DetailItem name={"Type(s):"} value={types} /></li>
+              <li><DetailItem name={"Evolutions:"} value={evolutions} /></li>
+            </ul>
+          </div>
+        </div>
+    );
+
 }
 
 class Details extends Component {
@@ -83,22 +103,36 @@ class Details extends Component {
                 id: null,
                 name: props.params.name
             },
+            species: null,
             evolutionChain: null,
         };
     }
 
     componentDidMount(){
         // get the initial list of pokemon for the app
-        pokeapi.getPokemonByName(this.props.params.name)
-            .then((response) => {
-                this.setState({pokemon: response} );
-                getEvolutionChain(response, (response) => {
+        pokeapi.getPokemonByName(this.props.params.name).then((response) => {
+
+            this.setState({pokemon: response} );
+
+            pokeapi.getPokemonSpeciesByName(response.name).then((response) => {
+
+                this.setState({species: response });
+
+                pokeapi.resource(response.evolution_chain.url).then((response) => {
 
                     this.setState({evolutionChain: response});
-                });
+
+                }).catch((err) => {
+                    console.error("Couldn't load pokemon evolution chain :(");
+                });;
+
             }).catch((err) => {
-                console.error("Couldn't load pokemon list from poke api :(");
+                console.error("Couldn't load pokemon species :(");
             });;
+
+        }).catch((err) => {
+            console.error("Couldn't load pokemon list from poke api :(");
+        });;
     }
 
     render() {
@@ -112,6 +146,7 @@ class Details extends Component {
                   </div>
                 </div>
                 <Info pokemon={this.state.pokemon}
+                      species={this.state.species}
                       evolutionChain={this.state.evolutionChain}/>
               </div>
             </div>
