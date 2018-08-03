160d12b80f16b5aa93eee5118c52e00a6c0642b2
David Arias <arias1011@gmail.com>
2018-08-03 17:57:39 +0200

    minor css changes: footer at the bottom, boxes background color and h1 link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router';
+
 class App extends Component {
 
     render() {
         return (
             <div className="wrap">
-              <header>
-                <div className="container">
+              <div className="content">
+                <div className="main-content">
+                  <header>
+                    <div className="container">
 
-                  <h1>Pokedex</h1><small>beta!</small>
+                      <h1>
+                        <Link to="/list/1">Pokedex</Link>
+                      </h1>
+                      <small>beta!</small>
+                    </div>
+
+                  </header>
+                  <div className="container">
+                    {this.props.children}
+                  </div>
                 </div>
-
-              </header>
-              <div className="container">
-                {this.props.children}
               </div>
-
               <footer>
                 <p>Made using only high quality bits.</p>
-                <p>Copyright &copy 2018 <a href="http://davidarias.net">David Arias</a>. All rights reserved. </p>
+                <p>Copyright (C) 2018 <a href="http://davidarias.net">David Arias</a>. All rights reserved. </p>
               </footer>
             </div>
         );
